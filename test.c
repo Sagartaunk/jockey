fe393b9b9836312fff6ccdb08b4f1e27908b9aef
Sagar Taunk <sagartaunk@proton.me>
2026-09-05 17:48:57 +0530

    Test: Update test code generated via jokeyc as well as update jokeyc binary

---
+++ test.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+
+int main() {
+    {
+        unsigned char QdHZEF[25];
+        int jNQrma = 0;
+        int OCBHXp = 0;
+        unsigned int MHqcWxRo = 0x66f9525d;
+        while (MHqcWxRo != 0x4096b822) {
+            switch(MHqcWxRo) {
+                case 0x55643083: {
+                int XEKuRb = 0x4d57;
+                    while ((jNQrma < OCBHXp)) 
+    {
+        unsigned int HGQhwrkV = 0xc2c680d;
+        while (HGQhwrkV != 0x486fffa) {
+            switch(HGQhwrkV) {
+                case 0xc2c680d: {
+                    if ((jNQrma == 0)) 
+    {
+        unsigned int JYdXQPDC = 0x6dfd92a6;
+        while (JYdXQPDC != 0x2a100003) {
+            switch(JYdXQPDC) {
+                case 0x6dfd92a6: {
+                    unsigned char OrmfHl[] = {0x6f, 0x48, 0x4f, 0x52, 0x4f, 0x47, 0x52, 0x4f, 0x48, 0x41, 0x06, 0x47, 0x52, 0x06, 0x5c, 0x43, 0x54, 0x49, 0x26};
+                    for(int i=0; i<19; i++) OrmfHl[i] ^= 0x26;
+                    printf("%s\n", OrmfHl);
+                    JYdXQPDC = 0x2a100003;
+                    break;
+                }
+                default:
+                    JYdXQPDC = 0x2a100003;
+                    break;
+            }
+        }
+    }
+                    else 
+    {
+        unsigned int izGIRhii = 0xb1f6ca9;
+        while (izGIRhii != 0x7275830) {
+            switch(izGIRhii) {
+                case 0xb1f6ca9: {
+                    if ((jNQrma == 3)) 
+    {
+        unsigned char ectFTx[14];
+        unsigned int njeZIxZB = 0x7a30b6a3;
+        while (njeZIxZB != 0x588d5eb0) {
+            switch(njeZIxZB) {
+                case 0x35211f0e: {
+                int wvAKYP = 0x51fd;
+                    printf("%s\n", ectFTx);
+                    njeZIxZB = 0x588d5eb0;
+                    break;
+                }
+                case 0x7a30b6a3: {
+                    unsigned char tmp_ectFTx[] = {0x69, 0x5b, 0x1e, 0x56, 0x57, 0x4a, 0x1e, 0x4a, 0x56, 0x4c, 0x5b, 0x5b, 0x1f, 0x3e};
+                    for(int i=0; i<14; i++) ectFTx[i] = tmp_ectFTx[i] ^ 0x3e;
+                    njeZIxZB = 0x35211f0e;
+                    break;
+                }
+                default:
+                    njeZIxZB = 0x588d5eb0;
+                    break;
+            }
+        }
+    }
+                    else 
+    {
+        unsigned int twPaGEAe = 0xfbf4a93;
+        while (twPaGEAe != 0x5eacf0c6) {
+            switch(twPaGEAe) {
+                case 0xfbf4a93: {
+                    printf("%d\n", jNQrma);
+                int kXwiqK = 0xfba3;
+                    twPaGEAe = 0x5eacf0c6;
+                    break;
+                }
+                default:
+                    twPaGEAe = 0x5eacf0c6;
+                    break;
+            }
+        }
+    }
+                int jhuEvY = 0xa0f2;
+                    izGIRhii = 0x7275830;
+                    break;
+                }
+                default:
+                    izGIRhii = 0x7275830;
+                    break;
+            }
+        }
+    }
+                    HGQhwrkV = 0x3815939d;
+                    break;
+                }
+                case 0x3815939d: {
+                int nByDPc = 0xe8db;
+                    jNQrma = (jNQrma + 1);
+                    HGQhwrkV = 0x486fffa;
+                    break;
+                }
+                default:
+                    HGQhwrkV = 0x486fffa;
+                    break;
+            }
+        }
+    }
+                int smGMyX = 0xa5a8;
+                    MHqcWxRo = 0x4096b822;
+                    break;
+                }
+                case 0x585997b9: {
+                    jNQrma = 0;
+                    MHqcWxRo = 0xad7ead6;
+                    break;
+                }
+                case 0x66f9525d: {
+                    unsigned char tmp_QdHZEF[] = {0x22, 0x1e, 0x1f, 0x05, 0x56, 0x06, 0x04, 0x19, 0x11, 0x04, 0x17, 0x1b, 0x56, 0x1e, 0x17, 0x05, 0x56, 0x05, 0x02, 0x17, 0x04, 0x02, 0x13, 0x12, 0x76};
+                    for(int i=0; i<25; i++) QdHZEF[i] = tmp_QdHZEF[i] ^ 0x76;
+                int jChqvT = 0x8d38;
+                    MHqcWxRo = 0x585997b9;
+                    break;
+                }
+                case 0xad7ead6: {
+                    OCBHXp = 5;
+                int cHCuFW = 0xe49f;
+                    MHqcWxRo = 0x55643083;
+                    break;
+                }
+                default:
+                    MHqcWxRo = 0x4096b822;
+                    break;
+            }
+        }
+    }
+    return 0;
+}
+
